c67c36f1116d15b6d333da3400b81b402c87251f
juanco <wivercr@gmail.com>
2022-07-08 16:54:03 -0600

    First lessons and challenge

--- src/test/java/com/serenitydojo/whenWorkingWithLists.java
+++ src/test/java/com/serenitydojo/whenWorkingWithLists.java
@@ -1,4 +1,49 @@
 package com.serenitydojo;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class whenWorkingWithLists {
-}
+
+    @Test
+    public void creatingAsimpleList() {
+
+        List myList = new ArrayList();
+        myList.add("Some string value");
+    }
+    @Test
+    public void creatingATypedList(){
+
+        List<String> names = new ArrayList<>();//you can remove and add items. Not immutable. generics
+        names.add("Juan");
+        names.add("Joe");
+        names.add("Someone");
+        names.remove("Joe");
+
+        for (int i = 0; i<2; i++) {
+            System.out.println(names.get(i));
+        }
+
+        List<Integer> ages = Arrays.asList(1,2,3,4);//quicker way to create and add items to an array, IMMUTABLE LIST.
+
+        for (int i = 0; i<4; i++) {
+            System.out.println(ages.get(i));
+        }
+    }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
